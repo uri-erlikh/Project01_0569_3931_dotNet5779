107ede23df5e1e9fa1378f5817188ec6542e6a87
moshew1991 <moshew1991@gmail.com>
2018-12-27 12:43:12 +0200

    פקטורי בBL ותחילה של הUI

---
+++ Project01_0569_3931_dotNet5779/BL/BL_Factory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public static class BL_Factory
+    {
+        public static IBL GetBL ()
+        {
+            return new BLImplementation();
+        }
+    }
+}
