b7d705fcdfb02e753c418c346956f1c9fab01c78
uriel erlikh <urier3@gmail.com>
2019-01-06 15:10:12 +0200

    addtest

--- Project01_0569_3931_dotNet5779/PLWPF/AddTestWindow.xaml.cs
+++ Project01_0569_3931_dotNet5779/PLWPF/AddTestWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BL;
 
 namespace PLWPF
 {
@@ -19,17 +20,29 @@ namespace PLWPF
     /// </summary>
     public partial class AddTestWindow : Window
     {
+        BO.Test test = new BO.Test();
+        BL.IBL bl;
+
         public AddTestWindow()
         {
             InitializeComponent();
+            
+            this.DataContext = test;
+            
+            bl = BL.BL_Factory.GetBL();
+
+            this.vehicleComboBox.ItemsSource = Enum.GetValues(typeof(BO.Vehicle));
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void AddTestButton_Click(object sender, RoutedEventArgs e)
         {
-
-            System.Windows.Data.CollectionViewSource testViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("testViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // testViewSource.Source = [generic data source]
+            try
+            {
+                bl.AddTest(test);
+                test = new BO.Test();
+                this.DataContext = test;
+            }
+            catch () { }
         }
     }
 }
