1e1cae203a43350ecc72329d5768282918e399e0
uriel erlikh <urier3@gmail.com>
2019-01-08 11:47:10 +0200

    j

--- Project01_0569_3931_dotNet5779/PLWPF/TestsWindow.xaml.cs
+++ Project01_0569_3931_dotNet5779/PLWPF/TestsWindow.xaml.cs
@@ -27,16 +27,18 @@ namespace PLWPF
         private void ToAddaTest_Click(object sender, RoutedEventArgs e)
         {
             new AddTestWindow().Show();
+            this.Close();
         }
 
         private void ToUpdateTestResult_Click(object sender, RoutedEventArgs e)
         {
-            new NumOfTestWindow().Show();
+            new UpdateTestWindow().Show();
         }
         
         private void ToPrintTest_Click(object sender, RoutedEventArgs e)
         {
-
+            new PrintTestWindow().Show();
+            this.Close();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
