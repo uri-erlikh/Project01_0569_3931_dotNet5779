daaddd34bbbf15b4ddf019a6af3dd56817eb7ec3
uriel erlikh <urier3@gmail.com>
2019-01-16 11:22:37 +0200

    xml

--- Project01_0569_3931_dotNet5779/PLWPF/AddTesterWindow.xaml.cs
+++ Project01_0569_3931_dotNet5779/PLWPF/AddTesterWindow.xaml.cs
@@ -106,16 +106,16 @@ namespace PLWPF
             this.Close();
         }
         //---------------------------------------------------------------
-        private void IDTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (this.IDTextBox.Text.Length < 9)
-                MessageBox.Show("please insert valid ID - 9 digits", "d.m.v.", MessageBoxButton.OK, MessageBoxImage.Warning);
-            if (int.TryParse(this.IDTextBox.Text, out int number) != true)
-            {
-                MessageBox.Show("Please enter only numbers", "d.m.v.", MessageBoxButton.OK, MessageBoxImage.Error);
-                this.IDTextBox.Clear();
-            }
-        }
+        //private void IDTextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (this.IDTextBox.Text.Length < 9)
+        //        MessageBox.Show("please insert valid ID - 9 digits", "d.m.v.", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    else if (int.TryParse(this.IDTextBox.Text, out int number) != true)
+        //    {
+        //        MessageBox.Show("Please enter only numbers", "d.m.v.", MessageBoxButton.OK, MessageBoxImage.Error);
+        //        this.IDTextBox.Clear();
+        //    }
+        //}
         //------------------------------------------------------------------
         //private void IDTextBox_TextChanged(object sender, TextChangedEventArgs e)
         //{
