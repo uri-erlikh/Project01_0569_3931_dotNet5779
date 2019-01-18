16a90756ee2206ce231f58e198b03662e64f005b
moshew1991 <moshew1991@gmail.com>
2019-01-18 14:10:04 +0200

    ax

--- Project01_0569_3931_dotNet5779/DAL/DAL_XML_IMP.cs
+++ Project01_0569_3931_dotNet5779/DAL/DAL_XML_IMP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ using System.Xml.Linq;
 namespace DAL
 {
 
-    class DAL_XML_IMP
+    class DAL_XML_IMP// : IDal
     {
         static DAL_XML_IMP instance = null;
 
@@ -36,7 +37,7 @@ namespace DAL
             else
                 LoadData();
         }
-
+        //------------------------------------------------------------
         private void CreateFiles()
         {
             testerRoot = new XElement("testers");
@@ -54,5 +55,29 @@ namespace DAL
                 throw new KeyNotFoundException("File upload problem");
             }
         }
+        //-----------------------------------------------------------------------------
+        public void AddTester(DO.Tester tester, bool[,] matrix)
+        {
+            XElement id = new XElement("id", tester.ID);
+            XElement privateName = new XElement("privateName", tester.PrivateName);
+            XElement familyName = new XElement("familyName", tester.FamilyName);
+            XElement dayOfBirth = new XElement("dayOfBirth", tester.DayOfBirth);
+            XElement personGender = new XElement("personGender", tester.PersonGender);
+            XElement phone = new XElement("phone", tester.Phone);
+            XElement testerExperience = new XElement("testerExperience", tester.TesterExperience);
+            XElement maxWeeklyTests = new XElement("maxWeeklyTests", tester.MaxWeeklyTests);
+            XElement testerVehicle = new XElement("testerVehicle", tester.TesterVehicle);
+            XElement rangeToTest = new XElement("rangeToTest", tester.RangeToTest);
+            XElement city = new XElement("city", tester.PersonAddress.City);
+            XElement street = new XElement("street", tester.PersonAddress.Street);
+            XElement numOfBuilding = new XElement("numOfBuilding", tester.PersonAddress.NumOfBuilding);
+            XElement personAddress = new XElement("personAddress", city, street, numOfBuilding);
+            XElement person = new XElement("person", id, privateName, familyName, dayOfBirth, personGender, phone, personAddress);
+            XElement schdule = new XElement("schdule", matrix);
+
+            testerRoot.Add(new XElement("tester", person, testerExperience, maxWeeklyTests, testerVehicle, rangeToTest));
+            testerRoot.Save(testerPath);
+        }
+        //---------------------------------------------------------------------------------------------------------------------------
     }
 }
